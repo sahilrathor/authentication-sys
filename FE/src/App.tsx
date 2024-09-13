ef657128085689483179d5debba75d8b4b843510
Sahil Rathor <sr7015342@gmail.com>
2024-09-14 00:44:08 +0530

    initial commit

---
+++ FE/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Auth from './pages/auth/Auth'
+// import myTheme from './utils/myTheme'
+
+export default function App() {
+  return (
+            <div className="w-full h-[100dvh] bg-emerald-800 text-slate-100 center-items">
+
+    <Auth/>
+
+    </div>
+  )
+}
